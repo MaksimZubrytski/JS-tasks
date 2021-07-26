b70b08bdfadf5f7a8bc8cea9088f3a28c07b8cfa
MaksimZubrytski <maksimxdd@gmail.com>
2021-07-26 10:21:35 +0300

    feat: added solved task 3

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,8 +1,10 @@
 /* Task 1 */
 function rememberResult(initialValue) {
   let savedValue = initialValue;
+
   return function (callback) {
     savedValue = callback(savedValue);
+
     return savedValue;
   };
 }
@@ -20,13 +22,15 @@ callWithRememberedResult(doubleValue); // => 16
 /* Task 2 */
 function callMaxTimes(numberOfTimes, callback) {
   let maximumNumberOfFunctionCalls = numberOfTimes;
+
   return function () {
     if (maximumNumberOfFunctionCalls === 0) {
-      maximumNumberOfFunctionCalls = 0;
-    } else {
-      callback();
-      maximumNumberOfFunctionCalls -= 1;
+      return null;
     }
+
+    maximumNumberOfFunctionCalls -= 1;
+
+    return callback();
   };
 }
 
@@ -54,8 +58,7 @@ function greet(greeting, name) {
 }
 
 const sayHelloTo = partial(greet, 'Hello');
-
-console.log(sayHelloTo('everyone')); // => 'Hello everyone'
+sayHelloTo('everyone'); // => 'Hello everyone'
 
 /* Task 4 */
 function curry(fn) {
@@ -73,52 +76,81 @@ function curry(fn) {
   // HINT: fn.length should be used to get number of fn arguments
 }
 
-// Expected result
 function summ1(a, b, c) {
   return a + b + c;
 }
 
 const curriedSumm1 = curry(summ1);
-console.log(curriedSumm1(1)(2)(3)); // => 6
+
+// Expected result
+curriedSumm1(1)(2)(3); // => 6
 
 function summ2(a, b, c, d, e) {
   return a + b + c + d + e;
 }
+
 const curriedSumm2 = curry(summ2);
+
+// Expected result
 curriedSumm2(1)(2)(3)(4)(5); // => 15
 
 /* Task 5 */
-const debounce = function (fn, timeOut) {
+let debounce = function (fn, timeOut) {
   return setTimeout(fn, timeOut);
 };
 
-// Expected result
 function dateNow() {
   console.log(Date.now());
 }
 
-function work(fn) {
+function CallTracking(fn) {
   let isCalled = false;
   let timerId;
   return function (func, time) {
     if (isCalled) {
       clearTimeout(timerId);
     }
+
     timerId = fn.call(this, func, time);
 
     isCalled = true;
   };
 }
 
-/*
-// First case
-debounce(dateNow, 1000); // => would be called in 1 second
-*/
-// ...
+debounce = CallTracking(debounce);
 
 // Second case
-/*
 debounce(dateNow, 100); // => canceled
 debounce(dateNow, 150); // => canceled
 debounce(dateNow, 170); // => would be called only last, previous would be canceled
-*/
+
+/* Task 6 */
+function memoize(fn) {
+  // TODO: implement function body
+  const cache = new Map();
+
+  return function (...args) {
+    const key = [].join.call(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = fn.apply(this, args);
+    cache.set(key, result);
+
+    return result;
+  };
+}
+
+// Expected result
+function summ(a, b, c) {
+  return a + b + c;
+}
+
+const memoizedSumm = memoize(summ);
+
+memoizedSumm(1, 2, 3); // => function summ was called, result 6
+memoizedSumm(1, 2, 3); // => function summ was NOT called, result 6 was remembered for arguments 1, 2, 3 and returned
+memoizedSumm(4, 2, 3); // => function summ was called, result 9
+memoizedSumm(4, 2, 3); // => function summ was NOT called, result 9 was remembered for arguments 4, 2, 3 and returned
