1ae7d0a504f722c9aa241e55cf1dbbc140a11a38
MaksimZubrytski <maksimxdd@gmail.com>
2021-07-28 15:06:08 +0300

    feat: add solved second task

--- tasks/task4.js
+++ tasks/task4.js
@@ -11,23 +11,69 @@ function createObject(obj) {
   return obj;
 }
 
-// Expected result
-const obj1 = {
-  testField: 1,
-  getTestField() {
-    return this.testField;
-  },
+/* Task 2 */
+function ItemCollection(item) {
+  this.item = item;
+  this.removedItem = null;
+
+  this.update = function (cb) {
+    Object.seal(this.item);
+    cb(this.item);
+    return this;
+  };
+
+  this.read = function () {
+    return this.item;
+  };
+
+  this.remove = function () {
+    this.removedItem = this.item;
+    this.item = null;
+    return this.removedItem;
+  };
+}
+
+function ItemsCollection(items) {
+  this.items = items;
+
+  this.update = function (cb) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      cb(this.items[i], i);
+    }
+
+    return this;
+  };
+
+  this.read = function () {
+    return this.items;
+  };
+
+  this.remove = function () {
+    this.items.pop();
+  };
+}
+
+function Collection(Constructor) {
+  this.data = [];
+
+  this.readAll = function () {
+    return this.data;
+  };
+
+  this.add = function (...args) {
+    this.data.push(new Constructor(...args));
+  };
+
+  this.get = function (cb) {
+    return new ItemCollection(this.data.find(cb));
+  };
+
+  this.getBy = function (cb) {
+    return new ItemsCollection(this.data.filter(cb));
+  };
+}
+
+module.exports = {
+  createObject,
+  Collection,
 };
-const getTestField1 = obj1.getTestField;
-console.log(getTestField1()); // => undefined, context was lost
-
-const obj2 = createObject({
-  testField: 1,
-  getTestField() {
-    console.log(this);
-    return this.testField;
-  },
-});
-
-const getTestField2 = obj2.getTestField;
-console.log(getTestField2()); // => 1, context was binded by createObject function
