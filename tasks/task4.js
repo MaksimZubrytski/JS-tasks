8b77d95dc7f4be946812e9b64a479299f7e87c31
MaksimZubrytski <maksimxdd@gmail.com>
2021-07-28 17:13:17 +0300

    refactor: task 2

--- tasks/task4.js
+++ tasks/task4.js
@@ -12,9 +12,10 @@ function createObject(obj) {
 }
 
 /* Task 2 */
-function ItemCollection(item) {
+function CollectionItem(item, data, index) {
   this.item = item;
-  this.removedItem = null;
+  this.data = data;
+  this.index = index;
 
   this.update = function (cb) {
     Object.seal(this.item);
@@ -27,34 +28,14 @@ function ItemCollection(item) {
   };
 
   this.remove = function () {
-    this.removedItem = this.item;
-    this.item = null;
-    return this.removedItem;
-  };
-}
-
-function ItemsCollection(items) {
-  this.items = items;
-
-  this.update = function (cb) {
-    for (let i = 0; i < this.items.length; i += 1) {
-      cb(this.items[i], i);
-    }
-
-    return this;
-  };
-
-  this.read = function () {
-    return this.items;
-  };
-
-  this.remove = function () {
-    this.items.pop();
+    this.data.splice(index, 1);
+    return this.item;
   };
 }
 
 function Collection(Constructor) {
   this.data = [];
+  this.collectionItems = null;
 
   this.readAll = function () {
     return this.data;
@@ -65,13 +46,74 @@ function Collection(Constructor) {
   };
 
   this.get = function (cb) {
-    return new ItemCollection(this.data.find(cb));
+    return new CollectionItem(
+      this.data.find(cb),
+      this.data,
+      this.data.findIndex(cb),
+    );
   };
 
   this.getBy = function (cb) {
-    return new ItemsCollection(this.data.filter(cb));
+    this.collectionItems = this.data.filter(cb);
+    return this;
+  };
+
+  this.update = function (cb) {
+    for (let i = 0; i < this.collectionItems.length; i += 1) {
+      cb(this.collectionItems[i], i);
+    }
+
+    return this;
+  };
+
+  this.read = function () {
+    return this.collectionItems;
+  };
+
+  this.remove = function (index) {
+    this.data.splice(index, 1);
   };
 }
+
+// Expected result
+function Dog(id, name) {
+  this.id = id;
+  this.name = name;
+}
+
+const dogsCollection = new Collection(Dog);
+
+dogsCollection.readAll(); // => []
+
+dogsCollection.add(1, 'Test name1'); // Creates instance of Dog
+dogsCollection.add(2, 'Test name2'); // Creates instance of Dog
+dogsCollection.add(3, 'Test name3'); // Creates instance of Dog
+
+dogsCollection.readAll(); // => array [Dog instance(id = 1, name = 'Test name1'), Dog instance(id = 2, name = 'Test name2'), Dog instance(id = 3, name = 'Test name3')]
+
+const dogsCollectionItem = dogsCollection.get(dog => dog.id === 1);
+
+dogsCollectionItem
+  .update(dog => {
+    dog.name = 'Updated test name 1'; // => Dog instance(id = 1, name = 'Updated test name 1')
+  })
+  .update(dog => {
+    dog.someNewField = 'Some new field'; // => nothing changed, collection items don't support initial model's extension (HINT: Object.seal should be used)
+  });
+
+const dog1 = dogsCollectionItem.read(); // => returns Dog instance object(id = 1, name = 'Test name1')
+
+const dogsCollectionItems = dogsCollection.getBy(dog => dog.id < 3);
+
+dogsCollectionItems.update((dog, index) => {
+  // => callback iterates through all matches collection's items ([Dog instance(id = 1, name = 'Test name1'), Dog instance(id = 2, name = 'Test name2')])
+  dog.name = `Updated test name ${index}`;
+});
+
+const dogs = dogsCollectionItems.read(); // => array [Dog instance(id = 1, name = 'Test name1'), Dog instance(id = 2, name = 'Test name2')]
+
+dogsCollectionItem.remove(); // => removes Dog instance object(id = 1, name = 'Test name1') from collection
+dogsCollection.remove(1);
 
 module.exports = {
   createObject,
